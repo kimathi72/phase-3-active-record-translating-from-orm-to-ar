d7d68c123204da5b7b74edd456fb1427548adc94
roy kimathi <kimathiwaweru@gmail.com>
2023-06-10 10:01:24 +0300

    done with assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
-class Dog
+
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/students.sqlite"
+)
+
+sql = <<-SQL
+    CREATE tABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT,
+    )
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
 end
